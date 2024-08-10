2d9a9ffbc3059a9a1439b41eb1c0dc85b1ea888e
skashequr <learningwithextracare@gmail.com>
2024-08-11 01:18:29 +0600

    add location search.

--- app/components/searchInput/searchInput.js
+++ app/components/searchInput/searchInput.js
@@ -24,7 +24,7 @@ const LocationAndFoodInput = () => {
                 >
                     <h2 className="text-lg font-semibold">Location:</h2>
                    
-                    <LocationInput></LocationInput>
+                    <LocationInput ></LocationInput>
                 </div>
 
                 {/* Right Side for Food Item */}
