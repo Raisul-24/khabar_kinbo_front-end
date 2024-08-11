15e924a393b33e248438a25166d3769dee222e7c
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-08-11 23:58:43 +0600

    add restaurant banner

---
+++ app/(main)/addRestaurant/page.js
@@ -0,0 +1,12 @@
+import Banner1 from './banner'
+const addRestaurant = () => {
+   return (
+      <div>
+         <Banner1></Banner1>
+         addRestaurant\
+         fwebwk
+      </div>
+   );
+};
+
+export default addRestaurant;
