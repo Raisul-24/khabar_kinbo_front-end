95275e42f5111b284643b5010e537d55fe0289f7
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-08-13 01:33:12 +0600

    complete add restaurants page

--- app/(main)/addRestaurant/banner.js
+++ app/(main)/addRestaurant/banner.js
@@ -1,10 +1,11 @@
 "use client"
+import Link from 'next/link';
 import React from 'react';
 
 const banner = () => {
   return (
     <div
-      className="hero min-h-96 rounded-b-xl"
+      className="hero min-h-96 rounded-b-xl max-w-screen"
       style={{
         backgroundImage: "url(/addRestaurentBanner1.jpg)",
       }}>
@@ -12,18 +13,18 @@ const banner = () => {
       <div className="hero-content text-neutral-content text-left">
         <div className="max-w-4xl">
           <h1 className="mb-5 text-xl md:text-3xl lg:text-5xl font-bold text-yellow-900">Partner with Khabar Kinbo <br></br>
-            at 0% commission for the 1st month!</h1>
-          <p className="mb-5 font-bold text-white text-sm md:text-xl">
+           <span className='text-sm md:text-xl lg:text-4xl'> at 0% commission for the 1st three month!</span></h1>
+          <p className="mb-5 font-bold text-white text-xs md:text-sm lg:text-xl">
             And get ads worth 5k BDT. Valid for new restaurant partners in select cities.
           </p>
-          <div className='grid grid-cols-1 lg:grid-cols-2 lg:gap-5'>
-          <button className="font-semibold border-2 px-3 py-2 rounded-xl border-yellow-700 hover:border-red-600 bg-yellow-50 hover:bg-white-100 text-red-400 hover:text-yellow-900"
-               >Register your restaurant</button>
-             <button className="font-semibold border-2 px-3 py-2 rounded-xl border-yellow-700 hover:border-red-600 bg-red-50 hover:bg-white-100 text-red-400 hover:text-yellow-900"
-               >Login to view your existing restaurant</button>  
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-5 text-xs md:text-base'>
+          <Link href='/'><button className="font-semibold border-2 px-3 py-2 rounded-xl border-yellow-700 hover:border-red-600 bg-yellow-50 hover:bg-white-100 text-red-400 hover:text-yellow-900"
+               >Register your restaurant</button></Link>
+             <Link href='/'><button className="font-semibold border-2 px-3 py-2 rounded-xl border-yellow-700 hover:border-red-600 bg-red-50 hover:bg-white-100 text-red-400 hover:text-yellow-900"
+               >Login to view your existing restaurant</button> </Link> 
           
           </div>
-          <p className='text-white text-center mt-5'>Need help? Contact +880 1558-323489</p>
+          <p className='text-white text-xs md:text-base text-center mt-5'>Need help? Contact +880 1558-323489</p>
         </div>
       </div>
     </div>
---
+++ app/(main)/addRestaurant/cards.js
@@ -0,0 +1,98 @@
+import Link from 'next/link';
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const cards = () => {
+   return (
+      <div className='my-20'>
+         <h1 className='text-lg md:text-3xl lg:text-5xl text-center font-bold md:font-extrabold'>Our Products</h1>
+         <Marquee speed='25' pauseOnHover='false'>
+            {/* card 1 */}
+         <div className="card card-compact bg-base-100 max-w-80 shadow-xl my-12 mx-5">
+            <figure>
+               <img className='w-full h-52'
+                  src="/addrestaurants/event.svg"
+                  alt="event" />
+            </figure>
+            <div className="card-body h-48">
+               <h2 className="card-title">Events</h2>
+               <p>Partner with us for Bangladesh’s grandest food & entertainment carnival - “Khabar-kinbo”...</p>
+               <div className="card-actions justify-end">
+                  <Link href='/'><button className='font-bold text-amber-700 hover:text-red-900'>Learn More</button></Link>
+               </div>
+            </div>
+         </div>
+
+         {/* card 2 */}
+         <div className="card card-compact bg-base-100 max-w-80 shadow-xl my-12 mx-5">
+            <figure>
+               <img className='w-full h-52'
+                  src="/addrestaurants/listing.jpg"
+                  alt="listing" />
+            </figure>
+            <div className="card-body h-48">
+               <h2 className="card-title">Listings</h2>
+               <p>A free app that allows you to manage your Khabar-kinbo listing directly from your smartphone</p>
+               <div className="card-actions justify-end">
+                  <Link href='/'><button className='font-bold text-amber-700 hover:text-red-900'>Learn More</button></Link>
+               </div>
+            </div>
+         </div>
+
+
+         {/* card 3 */}
+         <div className="card card-compact bg-base-100 max-w-80 shadow-xl my-12 mx-5">
+            <figure>
+               <img className='w-full h-52'
+                  src="/addrestaurants/advertise.jpg"
+                  alt="advertise" />
+            </figure>
+            <div className="card-body h-48">
+               <h2 className="card-title">Advertise</h2>
+               <p>For every marketing dollar spent, Khabar-kinbo returns over 8x the investment...</p>
+               <div className="card-actions justify-end">
+                  <Link href='/'><button className='font-bold text-amber-700 hover:text-red-900'>Learn More</button></Link>
+               </div>
+            </div>
+         </div>
+
+         {/* card 4 */}
+         <div className="card card-compact bg-base-100 max-w-80 shadow-xl my-12 mx-5">
+            <figure>
+               <img className='w-full h-52'
+                  src="/addrestaurants/Hyperpure.svg"
+                  alt="event" />
+            </figure>
+            <div className="card-body h-48">
+               <h2 className="card-title">Hyperpure</h2>
+               <p>Supplies fresh and high quality ingredients to restaurant for serving delicious meals...</p>
+               <div className="card-actions justify-end">
+                  <Link href='/'><button className='font-bold text-amber-700 hover:text-red-900'>Learn More</button></Link>
+               </div>
+            </div>
+         </div>
+
+
+         {/* card 5 */}
+         <div className="card card-compact bg-base-100 max-w-80 shadow-xl my-12 mx-5">
+            <figure>
+               <img className='w-full h-52'
+                  src="/addrestaurants/delivery.jpg"
+                  alt="ordering" />
+            </figure>
+            <div className="card-body h-48">
+               <h2 className="card-title">Online Ordering</h2>
+               <p>Start taking orders online from millions of users near you and deliver with Khabar-kinbo..</p>
+               <div className="card-actions justify-end">
+                  <Link href='/'><button className='font-bold text-amber-700 hover:text-red-900'>Learn More</button></Link>
+               </div>
+            </div>
+         </div>
+         </Marquee>
+
+
+      </div>
+   );
+};
+
+export default cards;
